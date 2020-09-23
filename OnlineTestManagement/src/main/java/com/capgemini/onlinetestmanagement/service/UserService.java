020fe90dcdea813a8b1208fc1fadf95f0406d728
suresh12141723 <38286940+suresh12141723@users.noreply.github.com>
2020-09-23 19:56:52 +0530

    Update UserService.java

--- OnlineTestManagement/src/main/java/com/capgemini/onlinetestmanagement/service/UserService.java
+++ OnlineTestManagement/src/main/java/com/capgemini/onlinetestmanagement/service/UserService.java
@@ -6,15 +6,11 @@ import java.util.Optional;
 import com.capgemini.onlinetestmanagement.entity.User;
 
 public interface UserService {
-	
-	    //User details.......
+	    
 		public boolean addUser(User user);
 		public boolean editUser(User user);
 		public User getUserById(int userId);
 		public List<User> getAllUsers();
 		public void deleteTheatre(int userId);
-		
-		
-		
 
 }
