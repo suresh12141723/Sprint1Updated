2d6368880268f6c304b1829da05161f6abfa0126
suresh12141723 <38286940+suresh12141723@users.noreply.github.com>
2020-09-23 19:55:33 +0530

    Update UserExceptions.java

--- OnlineTestManagement/src/main/java/com/capgemini/onlinetestmanagement/exceptions/UserExceptions.java
+++ OnlineTestManagement/src/main/java/com/capgemini/onlinetestmanagement/exceptions/UserExceptions.java
@@ -2,9 +2,7 @@ package com.capgemini.onlinetestmanagement.exceptions;
 
 public class UserExceptions extends Exception{
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	public UserExceptions(String message) {
